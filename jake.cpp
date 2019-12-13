f1e7f8d5edf07e47a14a037eead85baf5f5a7b8d
joreilly <jacob.oreilly@ndus.edu>
2019-12-13 11:52:14 -0600

    Updated FSCAN method

--- jake.cpp
+++ jake.cpp
@@ -4,35 +4,38 @@
 using namespace std;
 
 int trackList[] = {55, 58, 39, 18, 90, 160, 150, 38, 184};
-int disk_size = 200; 
+int queueTrackList[5];
+int diskSize = 200; 
+
 
 //scan scheduling policy
 double scanScheduling(int headPosition) {
 	int tracksTraversed = 0;
-	int trackListLength = sizeof(trackList) / sizeof(trackList[0]);
+	int trackListLength = sizeof(queueTrackList) / sizeof(queueTrackList[0]);
 	int trackListAccd[trackListLength];
 	int positiveMove = 1;
 	int maxElement = 0;
 	for (int i = 0; i < trackListLength; i++) {
-		if (trackList[i] > maxElement) {
-			maxElement = trackList[i];
+		if (queueTrackList[i] > maxElement) {
+			maxElement = queueTrackList[i];
 		}
 	}
 	int minElement = diskSize;
 	for (int i = 0; i < trackListLength; i++) {
-		if (trackList[i] < minElement) {
-			minElement = trackList[i];
+		if (queueTrackList[i] < minElement) {
+			minElement = queueTrackList[i];
 		}
 	}
 	while (true) {
 		int tempCount = 0;
 		for (int i = 0; i < trackListLength; i++) {
-			if (headPosition == trackList[i] && trackListAccd[i] != 1) {
+			if (headPosition == queueTrackList[i] && trackListAccd[i] != 1) {
 				trackListAccd[i] = 1;
 			}
 			tempCount += trackListAccd[i];
 			if (tempCount == trackListLength) {
-				return (double)tracksTraversed / trackListLength;
+				// return (double)tracksTraversed / trackListLength;
+				return tracksTraversed;
 			}
 		}
 		if (headPosition < maxElement && positiveMove == 1) {
@@ -46,23 +49,16 @@ double scanScheduling(int headPosition) {
 	}
 }
 
-// double nStepScan(int nReqeusts, int headPosition) {
+// double nStepScan(int nQueues, int headPosition) {
 //     int tracksTraversed = 0;
 // 	int trackListLength = sizeof(trackList) / sizeof(trackList[0]);
-//     int queue1[trackListLength];
-//     int queue2[trackListLength];
-//     bool queueDecider = true;
-//     for (int i = 0; i < trackListLength; i++) {
-//         if(queueDecider) {
-//             queue1 = headPosition;
-//             scanScheduling(headPosition)
-//             queueDecider = !queueDecider
-//         } else {
-//             queue2 = headPosition;
-//             scanScheduling(headPosition)
-//             queueDecider = !queueDecider
-//         }
-//         tracksTraversed++;
+//     int queueArray[nQueues][trackListLength]
+//     int queueDecider = 0;
+//     for (int i = 0; i < nQueues; i++) {
+//         for(int j = 0; j < trackListLength; j++) {
+// 			queueArray[i][j] =
+// 			tracksTraversed++; 
+// 		}
 //     }
 //     return (double)tracksTraversed / trackListLength;
 // }
@@ -70,26 +66,38 @@ double scanScheduling(int headPosition) {
 double FSCAN(int headPosition) {
     int tracksTraversed = 0;
 	int trackListLength = sizeof(trackList) / sizeof(trackList[0]);
-    int queue1[trackListLength];
-    int queue2[trackListLength];
+    int queue1[trackListLength / 2];
+    int queue2[trackListLength / 2];
+
+	for(int i = 0; i < trackListLength; i++) {
+		if((trackListLength / 2) > i) {
+			queue1[i] = trackList[i];
+			queueTrackList[i] = queue1[i];
+		} else {
+			queue2[i] = trackList[i];
+			queueTrackList[i] = queue2[i];
+		}
+	}
+	
     bool queueDecider = true;
-    for (int i = 0; i < trackListLength; i++) {
+    while(true) {
         if(queueDecider) {
-            queue1 = headPosition;
-            scanScheduling(queue1[i]);
+            tracksTraversed += scanScheduling(headPosition);
             queueDecider = !queueDecider;
         } else {
-            queue2 = headPosition;
-            scanScheduling(queue2);
+            tracksTraversed += scanScheduling(headPosition);
             queueDecider = !queueDecider;
+			cout << (double)tracksTraversed / trackListLength;
+			return (double)tracksTraversed / trackListLength;
         }
-        tracksTraversed++;
+		
     }
-    return (double)tracksTraversed / trackListLength;
+	
 }
 
 int main() {
     int head = 50;
+	FSCAN(head);
     // cout << "Average seek length for FIFO scheduling is " << << ".\n";
     // cout << "Average seek length for LIFO scheduling is " << << ".\n";
 }
